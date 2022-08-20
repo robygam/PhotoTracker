a6ff2d7cd46bbe045269e024b1d4f4136dc7e490
Roberto Garcia <roberto@mywallst.com>
2022-08-20 10:27:53 +0200

    Initial Commit

---
+++ PhotoTracker/PhotoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoTrackerApp.swift
+//  PhotoTracker
+//
+//  Created by Roberto Garcia on 20/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct PhotoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
