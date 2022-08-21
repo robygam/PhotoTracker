b23e88659b82fd71ec39809debae0bf1355de9f2
Roberto Garcia <roberto@mywallst.com>
2022-08-21 18:20:47 +0200

    first commit

--- PhotoTracker/ContentView.swift
+++ PhotoTracker/ContentView.swift
@@ -6,16 +6,61 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    // 1
+    @StateObject var deviceLocationService = DeviceLocationService.shared
+
+    // 2
+    @State var tokens: Set<AnyCancellable> = []
+
+    // 3
+    @State var coordinates: (lat: Double, lon: Double) = (0, 0)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // 1
+        VStack {
+            Text("Latitude: \(coordinates.lat)")
+                .font(.largeTitle)
+            Text("Longitude: \(coordinates.lon)")
+                .font(.largeTitle)
+        }
+        // 2
+        .onAppear {
+            observeCoordinateUpdates()
+            observeDeniedLocationAccess()
+            deviceLocationService.requestLocationUpdates()
+        }
+    }
+    
+    // 1
+    func observeCoordinateUpdates() {
+        deviceLocationService.coordinatesPublisher
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                print("Handle \(completion) for error and finished subscription.")
+            } receiveValue: { coordinates in
+                self.coordinates = (coordinates.latitude, coordinates.longitude)
+            }
+            .store(in: &tokens)
+    }
+
+    // 2
+    func observeDeniedLocationAccess() {
+        deviceLocationService.deniedLocationAccessPublisher
+            .receive(on: DispatchQueue.main)
+            .sink {
+                print("Handle access denied event, possibly with an alert.")
+            }
+            .store(in: &tokens)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    
     static var previews: some View {
         ContentView()
     }
+    
 }
